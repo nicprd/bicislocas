f1546eb11ee7fb5d360fabaaf757cc4602ae1f05
lordjim <lordjim@none>
2019-08-02 01:53:54 +0200

    script whereaugoing muestra rutas posibles

--- scripts/whereugoing.py
+++ scripts/whereugoing.py
@@ -6,8 +6,21 @@ medimos la distancia entre las dos estaciones.
 Calculamos si es un tiempo razonable de destino.
 Asumimos como destino o seguimos esperando un nuevo destino
 """
+
+"""
+TODO: LA ruta sensata la mediremos en km/h 
+      Crearemos un conjunto de rutas sensatas para cada nueva ruta que se abre
+"""
+
 import requests as req
 import json
+from datetime import datetime
+#para medir distancias entre coordenadas
+import geopy.distance 
+
+#para ahorrar en calculos creamos esta variable global.
+#la inicializamos al definir get_all_stations aqui abajo, con su valor
+REFERENCE_STATIONS = {}
 
 def get_api_key():
     with open("../apikey.json", "r") as f:
@@ -20,15 +33,63 @@ def get_all_stations():
     url = api_url + "transport/bicimad/stations/"
     return req.get(url, headers = get_api_key()).json()["data"]
 
-stations = { i["id"]: [i["dock_bikes"], i["free_bases"]] for i in get_all_stations()}
+#################GLOBAL VARIABLE#############
+REFERENCE_STATIONS = get_all_stations()  ####
+#############################################
 
-print("[+] Resgistrando cambios en las estaciones")
+def get_stat_by_id():
+    #guardamos las estaciones como un diccionario { idNumerico : {}}
+    #asi se vuelve mas facil acceder a la estacion sin iterar toda la lista
+    return {
+        i["id"]:
+        {
+            "dock_bikes": i["dock_bikes"],
+            "free_bases" : i["free_bases"]
+        }
+        for i in get_all_stations()
+    }
 
+def find_new_travel():
+    stations = get_stat_by_id()
+    while(True):
+        a = get_all_stations()
+        for i in a:
+            s_id = i["id"]
+            if stations[s_id]["dock_bikes"] == (i["dock_bikes"] -1) :
+                print(f'[+] una bici se cogio en la estacion {i["name"]}')
+                return s_id
+
+
+def find_end_travel():
+    stations = get_stat_by_id()
+    while(True):
+        a = get_all_stations()
+        for i in a:
+            s_id = i["id"]
+            if stations[s_id]["free_bases"] == (i["free_bases"] -1) :
+                print(f'[!] una bici se dejo en la estacion {i["name"]}')
+                return s_id
+
+
+def get_location_id(sid):
+    #variable global que contiene una muestra de todas las estaciones
+    for i in REFERENCE_STATIONS: 
+        if i["id"] == sid:
+            return i["geometry"]["coordinates"]
+    raise "El id de estacion no existe"
+
+print("Encontrado un nuevo viaje: ")
+inicio = find_new_travel()
+ini_date = datetime.now()
+print(f'[ ] la fecha de inicio del viaje es {ini_date}')
+ini_loc = get_location_id(inicio)
+print(f'[ ] las coordenadas de inicio del viaje son {ini_loc}')
 while(True):
-    for i in get_all_stations():
-        if stations[i["id"]][0] == i["dock_bikes"] -1 :
-            print(f'[+] una bici se cogio en la estacion {i["name"]}')
-        if stations[i["id"]][1] == i["free_bases"] -1:
-            print(f'[-] una bici se dejo en la estacion {i["name"]}')
-    stations = { i["id"]: [i["dock_bikes"], i["free_bases"]] for i in get_all_stations()}
+    fin = find_end_travel()
+    end_date = datetime.now()
+    end_loc = get_location_id(fin)
+    dist = geopy.distance.distance(ini_loc, end_loc)
+    time = end_date - ini_date
+    print(f'[ ] posible fin de trayecto:')
+    print(f'[ ] recorridos {dist} minimo en {time} horas.')
 
